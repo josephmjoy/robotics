2d33c0ffe9758e105e5a838426442af4252b0b9f
Joseph Joy <josephmjoy@hotmail.com>
2018-03-27 09:52:36 -0700

    CommUtils: skeleton
    Placeholder for implementation.

--- java_robotutils/src/com/rinworks/robotutils/CommUtils.java
+++ java_robotutils/src/com/rinworks/robotutils/CommUtils.java
@@ -3,6 +3,69 @@
 
 package com.rinworks.robotutils;
 
+import java.util.function.BiConsumer;
+
+import com.rinworks.robotutils.RobotComm.DatagramTransport;
+
 public class CommUtils {
+    
+    public class EchoServer {
+        public EchoServer(String address, int port) {}
+        public void run() {}
+    }
+    
+    public class EchoClient {
+        public EchoClient(String localAddress, String remoteAddress, int remotePort) {
+            
+        }
+        public void run() {}
+    }
+
+    
+    public static DatagramTransport createUdpTransport() {
+        return new UdpTransport();
+    }
+    
+    
+    private static class UdpTransport implements DatagramTransport{
+
+
+        @Override
+        public Address resolveAddress(String address) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+
+
+        @Override
+        public RemoteNode newRemoteNode(Address remoteAddress) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public void close() {
+            // TODO Auto-generated method stub
+            
+        }
+
+
+
+        @Override
+        public void startListening(BiConsumer<String, RemoteNode> handler) {
+            // TODO Auto-generated method stub
+            
+        }
+
+
+
+        @Override
+        public void stopListening() {
+            // TODO Auto-generated method stub
+            
+        }
+        
+    }
 
 }
