1f2c33d77b514513a4692def6bed042740bb5156
josephmjoy <josephj@rinworks.com>
2019-01-05 01:27:42 -0800

    Remove most PEP8 warnings from test_msgmap.py
    Chiefly, added docstrings to methods.

--- python_robotutils/robotutils/misc/test_msgmap.py
+++ python_robotutils/robotutils/misc/test_msgmap.py
@@ -12,28 +12,31 @@ import random
 from . import msgmap
 
 class TestStringMethods(unittest.TestCase):
-
+    """Container class for unittest tests."""
 
     def test_empty_str_to_dict(self):
+        """Empty string to dict """
         d = msgmap.str_to_dict('')
         self.assertEqual(len(d), 0)
 
-
     def test_empty_dict_to_str(self):
+        """Empty dict to string"""
         s = msgmap.dict_to_str(dict())
         self.assertEqual(len(s), 0)
 
-
     def test_singleton_str_to_dict(self):
+        """single kv-pair to dict"""
         d = msgmap.str_to_dict('k:v')
-        self.assertEqual(len(d),1)
+        self.assertEqual(len(d), 1)
         self.assertEqual(d.get('k'), 'v')
 
     def test_singleton_dict_to_str(self):
+        """dict to single kv-pair"""
         s = msgmap.dict_to_str({'k':'v'})
         self.assertEqual(s, 'k:v')
 
     def test_simple_str_to_dict(self):
+        """ Simple case of multiple kv-pairs - str to dict"""
         d = msgmap.str_to_dict('k1:v1 k2:v2 k3:v3')
         self.assertEqual(len(d), 3)
         self.assertEqual(d.get('k1'), 'v1')
@@ -41,6 +44,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(d.get('k3'), 'v3')
 
     def test_simple_dict_to_str(self):
+        """ Simple case of multiple kv-pairs - dict to str"""
         s = msgmap.dict_to_str({'k1':'v1', 'k2':'v2', 'k3':'v3'})
         self.assertEqual(s, 'k1:v1 k2:v2 k3:v3')
 
@@ -70,10 +74,10 @@ class TestStringMethods(unittest.TestCase):
         #values = ['1', '2', '3']
 
         assert len(keys) == len(values)
-        kv_dirty  = [] # includes randome whitespace
+        kv_dirty = [] # includes randome whitespace
         kv_clean = []  # does not include any whitespace
         pre = ''
-        for (k,v) in zip(keys, values):
+        for (k, v) in zip(keys, values):
             # make sure our keys and values have no edge whitespaces. We
             # depend on this so that when we convert from dict to str we
             # get exactly what we predict.
@@ -106,7 +110,7 @@ class TestStringMethods(unittest.TestCase):
         # create a dictionary usig the 'dirty' meessage - which has random
         # whitespace inserted -- and verify that all the k:v mappings are there
         d = msgmap.str_to_dict(msg_dirty)
-        for (k,v) in zip(keys, values):
+        for (k, v) in zip(keys, values):
             self.assertEqual(v, d.get(k))
 
         # now convert back to a string and verify it is what we expect - the
@@ -115,72 +119,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(output_msg, msg_clean)
 
 def random_whitespace():
+    """Return a 'random' amount of 'random' whitespace characters"""
     whitespace = '    \t\t    \n\r      '
     i = random.randrange(len(whitespace))
     return whitespace[i:]
-
-
-# The following seems to have no purpose here, because
-# these tests are living at or below the robotutils package directory
-# if __name__ == '__main__':
-#    unittest.main()
-
-
-'''
-
-        assert(keys.length == values.length);
-
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sbClean = new StringBuilder();
-        String pre = "";
-        for (int i = 0; i< keys.length; i++) {
-            String k = keys[i];
-            String v = values[i];
-            // Make sure our test data is clean - no edge whitespace.
-            assert(k.equals(k.trim()));
-            assert(v.equals(v.trim()));
-            String space = randomWhitespace();
-            sb.append(space);
-
-            sbClean.append(pre);
-            sbClean.append(k);
-            pre = " ";
-            sb.append(k);
-            space = randomWhitespace();
-            sb.append(space);
-
-            sb.append(":");
-            sbClean.append(":");
-
-            space = randomWhitespace();
-            sb.append(space);
-
-            sb.append(v);
-            sbClean.append(v);
-
-            space = randomWhitespace();
-            sb.append(space);
-        }
-        String input = sb.toString();
-        String cleanInput = sbClean.toString();
-
-        HashMap<String, String> map = StructuredMessageMapper.toHashMap(input);
-
-        // Verify map contents.
-        for (int i=0; i<keys.length; i++) {
-            String k = keys[i];
-            String v = values[i];
-            assertEquals(map.get(k), v);
-        }
-
-        // Convert back to string
-        String outputMessage = StructuredMessageMapper.toString(map, keys);
-        assertEquals(outputMessage, cleanInput);
-    }
-
-    // Raturns a random amount of "random" whitespace.
-    String randomWhitespace() {
-        String randomWhitespace = "    \t\t    \n\r      ";
-        return randomWhitespace.substring((int)(Math.random()*randomWhitespace.length()));
-    }
-'''
