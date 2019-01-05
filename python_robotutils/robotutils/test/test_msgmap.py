d5dc466ac6dfe1652ac38913aa9bc2e18535ccf0
josephmjoy <josephj@rinworks.com>
2019-01-04 18:56:28 -0800

    Remove PEP8 warnings
    Chiefly indetation

--- python_robotutils/robotutils/test/test_msgmap.py
+++ python_robotutils/robotutils/test/test_msgmap.py
@@ -1,12 +1,15 @@
-#
-# Unit tests for the msgmap module
-# by JMJ - a port of the JUnit test for the corresponding Java
-# robotutils class, StructuredMessageMap.
-#
+'''
+Unit tests for the robotutils.msgmap module.
+
+This is a port of the JUnit test for the corresponding Java
+robotutils class, StructuredMessageMap.
+Author: JMJ
+'''
 
 import unittest
-import robotutils.msgmap as msgmap
 import random
+
+import robotutils.msgmap as msgmap
 
 class TestStringMethods(unittest.TestCase):
 
@@ -36,7 +39,7 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(d.get('k1'), 'v1')
         self.assertEqual(d.get('k2'), 'v2')
         self.assertEqual(d.get('k3'), 'v3')
-        
+
     def test_simple_dict_to_str(self):
         s = msgmap.dict_to_str({'k1':'v1', 'k2':'v2', 'k3':'v3'})
         self.assertEqual(s, 'k1:v1 k2:v2 k3:v3')
@@ -48,54 +51,54 @@ class TestStringMethods(unittest.TestCase):
             directions.
         '''
         keys = [
-                    "!#!@AB89.[],/-+",
-                    "2f0j20j0j",
-                    "13r2ffs,,,,,,",
-                    "-n339ghhss8898v",
-                    "d"
-        ];
-        
+            "!#!@AB89.[],/-+",
+            "2f0j20j0j",
+            "13r2ffs,,,,,,",
+            "-n339ghhss8898v",
+            "d"
+        ]
+
         values = [
-                    "13r \n\t \r doo bE B",
-                    "waka waka waka",
-                    "What's all the fuss?!",
-                    "(a=3.5, b=4.5, c=11.2)",
-                    "\"some quoted string\""
-        ];
-  
+            "13r \n\t \r doo bE B",
+            "waka waka waka",
+            "What's all the fuss?!",
+            "(a=3.5, b=4.5, c=11.2)",
+            "\"some quoted string\""
+        ]
+
         #keys  = ['a', 'b', 'c']
         #values = ['1', '2', '3']
-        
+
         assert len(keys) == len(values)
         kv_dirty  = [] # includes randome whitespace
         kv_clean = []  # does not include any whitespace
         pre = ''
         for (k,v) in zip(keys, values):
-                # make sure our keys and values have no edge whitespaces. We
-                # depend on this so that when we convert from dict to str we
-                # get exactly what we predict.
-                assert k == k.strip()
-                assert v == v.strip()
+            # make sure our keys and values have no edge whitespaces. We
+            # depend on this so that when we convert from dict to str we
+            # get exactly what we predict.
+            assert k == k.strip()
+            assert v == v.strip()
 
-                # This is to ensure that there is a space between each value and
-                # subsequent key
-                pk = pre + k
-                pre = ' '
+            # This is to ensure that there is a space between each value and
+            # subsequent key
+            pk = pre + k
+            pre = ' '
 
-                # note: random_whitespace may return an empty string
-                kv_dirty.append(random_whitespace())
-                kv_clean.append(pk)
-                kv_dirty.append(pk)
+            # note: random_whitespace may return an empty string
+            kv_dirty.append(random_whitespace())
+            kv_clean.append(pk)
+            kv_dirty.append(pk)
 
-                kv_dirty.append(random_whitespace())
-                kv_clean.append(':')
-                kv_dirty.append(':')
+            kv_dirty.append(random_whitespace())
+            kv_clean.append(':')
+            kv_dirty.append(':')
 
-                kv_dirty.append(random_whitespace())
-                kv_clean.append(v)
-                kv_dirty.append(v)
+            kv_dirty.append(random_whitespace())
+            kv_clean.append(v)
+            kv_dirty.append(v)
 
-                kv_dirty.append(random_whitespace())
+            kv_dirty.append(random_whitespace())
 
         msg_dirty = ''.join(kv_dirty)
         msg_clean = ''.join(kv_clean)
@@ -115,18 +118,18 @@ def random_whitespace():
     whitespace = '    \t\t    \n\r      '
     i = random.randrange(len(whitespace))
     return whitespace[i:]
-    
-        
+
+
 # The following seems to have no purpose here, because
 # these tests are living at or below the robotutils package directory
 # if __name__ == '__main__':
 #    unittest.main()
-        
+
 
 '''
-        
+
         assert(keys.length == values.length);
-        
+
         StringBuilder sb = new StringBuilder();
         StringBuilder sbClean = new StringBuilder();
         String pre = "";
@@ -138,43 +141,43 @@ def random_whitespace():
             assert(v.equals(v.trim()));
             String space = randomWhitespace();
             sb.append(space);
-            
+
             sbClean.append(pre);
             sbClean.append(k);
             pre = " ";
             sb.append(k);
             space = randomWhitespace();
             sb.append(space);
-            
+
             sb.append(":");
             sbClean.append(":");
-            
+
             space = randomWhitespace();
             sb.append(space);
-            
+
             sb.append(v);
             sbClean.append(v);
-            
+
             space = randomWhitespace();
             sb.append(space);
         }
         String input = sb.toString();
         String cleanInput = sbClean.toString();
-        
+
         HashMap<String, String> map = StructuredMessageMapper.toHashMap(input);
-        
+
         // Verify map contents.
         for (int i=0; i<keys.length; i++) {
             String k = keys[i];
             String v = values[i];
             assertEquals(map.get(k), v);
         }
-        
+
         // Convert back to string
         String outputMessage = StructuredMessageMapper.toString(map, keys);
         assertEquals(outputMessage, cleanInput);
     }
-    
+
     // Raturns a random amount of "random" whitespace.
     String randomWhitespace() {
         String randomWhitespace = "    \t\t    \n\r      ";
