afaf1fd504be28629a140a64bcf44a72adc559f2
josephmjoy <josephj@rinworks.com>
2019-01-29 02:16:08 -0800

    Fixed camelCase naming in robotcomm.py

--- python_robotutils/robotutils/comm/robotcomm.py
+++ python_robotutils/robotutils/comm/robotcomm.py
@@ -13,20 +13,16 @@ from . import _protocol
 from .channel import Channel
 
 
-
-# TODO: Fix these...
-# pylint: disable=invalid-name
-
 class RobotComm():
     """The top-level class for robotcomm"""
 
     def __init__(self, transport):
         """Initializes an instance of RobotComm with the specified transport."""
 
-        self.commClosed = False # set to false when close() is called.
+        self._closed = False # set to false when close() is called.
         self.listening = False  # Whether or not client is listening
         self.transport = transport
-        self.listenLock = threading.Lock()
+        self._listenlock = threading.Lock()
         self.rand = random.random
         self._channels = concurrent_helper.ConcurrentDict()
 
@@ -34,28 +30,28 @@ class RobotComm():
     # Public methods
     #
 
-    def new_channel(self, channelName) -> Channel:
+    def new_channel(self, channel_name) -> Channel:
         """Returns a new channel"""
-        if self.commClosed:
+        if self._closed:
             raise  ValueError("Robot comm is closed!")
-        if _protocol.containschars(channelName, _protocol.BAD_FIELD_CHARS):
+        if _protocol.containschars(channel_name, _protocol.BAD_FIELD_CHARS):
             raise ValueError("channel name has invalid characters: [{}]"
-                             .format(channelName))
+                             .format(channel_name))
 
         # We pass the Channel class as the function in the 2nd arg below! It
         # will be supplied with the initializer arguments if/when invoked.
-        chan, created = self._channels.upsert(channelName, Channel, self,
-                                              channelName, self.transport)
+        chan, created = self._channels.upsert(channel_name, Channel, self,
+                                              channel_name, self.transport)
         if not created:
             raise ValueError("Channel with name [{}] exists"
-                             .format(channelName))
+                             .format(channel_name))
         return chan
 
 
     def start_listening(self) -> None:
         """Start listening for messages on all channels"""
         start = False
-        with self.listenLock:
+        with self._listenlock:
             if not self.listening:
                 start = True
                 self.listening = True
@@ -67,7 +63,7 @@ class RobotComm():
     def stop_listening(self) -> None:
         """Stop listening"""
         stop = False
-        with self.listenLock:
+        with self._listenlock:
             if not self.listening:
                 self.listening = False
                 stop = True
@@ -85,7 +81,7 @@ class RobotComm():
     def close(self) -> None:
         """Close.  THis will cause subsequent attempts to create channels to
         fail with an ValueError exception."""
-        self.commClosed = True
+        self._closed = True
         self.stop_listening()
 
         # Close all channels
@@ -100,7 +96,7 @@ class RobotComm():
     def periodic_work(self):
         """ MUST be called periodically so that periodic maintenance tasks can be
         done, chiefly handling of re-transmits."""
-        if not self.commClosed and self.is_listening():
+        if not self._closed and self.is_listening():
             # pylint: disable=protected-access
             self._channels.process_all(lambda name, chan: chan._periodic_work())
 
@@ -118,7 +114,7 @@ class RobotComm():
     #
 
 
-    def _listen_handler(self, msg, rn):
+    def _listen_handler(self, msg, remotenode):
         """The handler Robotcomm provides to the underlying transport to handle
         incoming messages"""
         try:
@@ -138,18 +134,18 @@ class RobotComm():
             client = chan._client # pylint: disable=protected-access
             DgEnum = _protocol.DatagramType
             if dgram.dgType == DgEnum.RTCMD:
-                server.handle_received_RTCMD(dgram, rn)
+                server.handle_received_RTCMD(dgram, remotenode)
             elif dgram.dgType == DgEnum.RTCMDRESP:
-                client.handle_received_RTCMDRESP(dgram, rn)
+                client.handle_received_RTCMDRESP(dgram, remotenode)
             elif dgram.dgType == DgEnum.MSG:
                  # pylint: disable=protected-access
-                chan._handle_received_message(dgram, rn)
+                chan._handle_received_message(dgram, remotenode)
             elif dgram.dgType == DgEnum.CMD:
-                server.handle_received_CMD(dgram, rn)
+                server.handle_received_CMD(dgram, remotenode)
             elif dgram.dgType == DgEnum.CMDRESP:
-                client.handle_received_CMDRESP(dgram, rn)
+                client.handle_received_CMDRESP(dgram, remotenode)
             elif dgram.dgType == DgEnum.CMDRESPACK:
-                server.handle_received_CMDRESPACK(dgram, rn)
+                server.handle_received_CMDRESPACK(dgram, remotenode)
             else:
                 # we have already validated the message, so shouldn't get here.
                 assert False
