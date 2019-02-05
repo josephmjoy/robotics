b4c3c62d4e5d2b52b49881be7750f67fd71307ab
josephmjoy <josephj@rinworks.com>
2019-02-05 06:56:25 -0800

    Beefed up the basic udp transport test a bit
    Test verifies messages. Also bumped up count to 1000.

--- python_robotutils/robotutils/test_comm_helper.py
+++ python_robotutils/robotutils/test_comm_helper.py
@@ -29,25 +29,32 @@ class CommUtilsTest(unittest.TestCase):
 
     # Echo client-server tests use these channel names.
     ECHO_CHANNEL_A = "A"
+    MSG_PREFIX = "MSG"
 
     def test_udp_transport_simple(self):
         """Simple test of the UDP transport"""
+        # The client sends string versions of integers 1 to N
+        # The server parses and adds these up and in the end
+        # the test verifies that the sum is as expected.
         client = UdpTransport(MAX_PACKET_SIZE)
         server = UdpTransport(MAX_PACKET_SIZE,
                               local_host=SERVER_IP_ADDRESS,
                               local_port=SERVER_PORT)
-        count = 3
+        count = 1000
+        total = conc.AtomicNumber(0)
         latch = conc.CountDownLatch(count)
 
         def process_message(msg, node):
             _TRACE("Server got message [{}] from {}".format(msg, node))
+            val = int(msg[len(self.MSG_PREFIX):]) # skip past prefix
+            total.add(val)
             latch.count_down()
 
         server.start_listening(process_message)
 
         remote = client.new_remote_node(SERVER_IP_ADDRESS, SERVER_PORT)
-        for i in range(count):
-            msg = "Test message #" + str(i)
+        for i in range(1, count+1):
+            msg = self.MSG_PREFIX + str(i)
             client.send(msg, remote)
             _TRACE("Client sent message [{}]".format(msg))
             # time.sleep(0.1)
@@ -55,8 +62,11 @@ class CommUtilsTest(unittest.TestCase):
         _LOGGER.info("Waiting for all messages to arrive...")
         if latch.wait(1):
             _LOGGER.info("Done waiting for all %d messages to arrive.", count)
+            expected = count * (count + 1) // 2 # sum of 1 to count
+            self.assertEqual(total.value(), expected)
         else:
-            _LOGGER.info("TIMED OUT waiting for all %d messages to arrive.", count)
+            msg = "TIMED OUT waiting for all {} messages to arrive.".format(count)
+            self.fail(msg)
 
         client.close()
         server.close()
